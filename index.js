c8753a2c9af742b04710040309d261d7bf0ac024
+ <+>
2024-06-27 09:26:01 +0300

    intial arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1= 2;
+let num2=31;
+const multiply=((num1*=num2));
+
+let random =Math.floor(Math.random()*100)+1;
+
+let num3= 14;
+let num4= 5;
+let mod=((num3&num4));
+
+let numbers =[5,4,1,10];
+let max=(5+4+1+10);
